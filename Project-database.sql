68f99063be9ac88d46e291cc380fd41d5d706208
Emperor Anuku <emperoranuku@emperors-air.home>
2024-04-01 20:03:15 -0400

    Updated Column name from password to Securepassword

--- Project-database.sql
+++ Project-database.sql
@@ -5,11 +5,11 @@ USE Stocks_Website;
 CREATE TABLE IF NOT EXISTS Users (
     userID INT(11) AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50),
-    password VARCHAR(50),
+    Securepassword VARCHAR(50),
     lastlogin DATE,
     permissions INT(3)
 );
-INSERT INTO Users (username, password, permissions)
+INSERT INTO Users (username, Securepassword, permissions)
 VALUES 
 /* Admin has permission level 3 and customer has permissio level 2 */
     ('admin', 'password', 3),    
